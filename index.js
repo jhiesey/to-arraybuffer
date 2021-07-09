4ec6c0d8528d8609f675048b9f6fa8f7642d2a4b
Jimmy Wärting <jimmy@warting.se>
2021-07-09 18:34:50 +0200

    fix indentation

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ module.exports = buf => {
   if (!ArrayBuffer.isView(buf)) throw new Error('Argument must be a ArrayBufferView')
 
   // If the buffer isn't a subarray, return the underlying ArrayBuffer
-	// Otherwise we need to get a proper copy
+  // Otherwise we need to get a proper copy
   return buf.byteLength === buf.buffer.byteLength
 	  ? buf.buffer
     : buf.buffer.slice(buf.byteOffset, buf.byteOffset + buf.byteLength)
