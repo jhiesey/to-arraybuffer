c9f9ee2178f9e0f0648225476093ea9629a70d0f
Jimmy Wärting <jimmy@warting.se>
2021-07-09 18:09:23 +0200

    unnecessary check

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ module.exports = buf => {
   if (!ArrayBuffer.isView(buf)) throw new Error('Argument must be a ArrayBufferView')
 
   // If the buffer isn't a subarray, return the underlying ArrayBuffer
-  if (buf.byteOffset === 0 && buf.byteLength === buf.buffer.byteLength) {
+  if (buf.byteLength === buf.buffer.byteLength) {
     return buf.buffer
   }
 
