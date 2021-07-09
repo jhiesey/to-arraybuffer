94b5d70bb771140bf29044cb3deeff8221b2a267
Jimmy Wärting <jimmy@warting.se>
2021-07-09 18:06:16 +0200

    standard --fix

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 module.exports = buf => {
-	if (buf instanceof ArrayBuffer) return buf
+  if (buf instanceof ArrayBuffer) return buf
 
-	if (!ArrayBuffer.isView(buf)) throw new Error('Argument must be a ArrayBufferView')
+  if (!ArrayBuffer.isView(buf)) throw new Error('Argument must be a ArrayBufferView')
 
-	// If the buffer isn't a subarray, return the underlying ArrayBuffer
-	if (buf.byteOffset === 0 && buf.byteLength === buf.buffer.byteLength) {
-		return buf.buffer
-	}
+  // If the buffer isn't a subarray, return the underlying ArrayBuffer
+  if (buf.byteOffset === 0 && buf.byteLength === buf.buffer.byteLength) {
+    return buf.buffer
+  }
 
-	// Otherwise we need to get a proper copy
-	return buf.buffer.slice(buf.byteOffset, buf.byteOffset + buf.byteLength)
+  // Otherwise we need to get a proper copy
+  return buf.buffer.slice(buf.byteOffset, buf.byteOffset + buf.byteLength)
 }
