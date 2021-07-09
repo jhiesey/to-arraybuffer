dc8ea663f0d3722043dc36c5f1aac23680b15413
Jimmy Wärting <jimmy@warting.se>
2021-07-09 17:54:00 +0200

    Remove buffer entirely
    closes #1

--- index.js
+++ index.js
@@ -1,27 +1,13 @@
-var Buffer = require('buffer').Buffer
+module.exports = buf => {
+	if (buf instanceof ArrayBuffer) return buf
 
-module.exports = function (buf) {
-	// If the buffer is backed by a Uint8Array, a faster version will work
-	if (buf instanceof Uint8Array) {
-		// If the buffer isn't a subarray, return the underlying ArrayBuffer
-		if (buf.byteOffset === 0 && buf.byteLength === buf.buffer.byteLength) {
-			return buf.buffer
-		} else if (typeof buf.buffer.slice === 'function') {
-			// Otherwise we need to get a proper copy
-			return buf.buffer.slice(buf.byteOffset, buf.byteOffset + buf.byteLength)
-		}
+	if (!ArrayBuffer.isView(buf)) throw new Error('Argument must be a ArrayBufferView')
+
+	// If the buffer isn't a subarray, return the underlying ArrayBuffer
+	if (buf.byteOffset === 0 && buf.byteLength === buf.buffer.byteLength) {
+		return buf.buffer
 	}
 
-	if (Buffer.isBuffer(buf)) {
-		// This is the slow version that will work with any Buffer
-		// implementation (even in old browsers)
-		var arrayCopy = new Uint8Array(buf.length)
-		var len = buf.length
-		for (var i = 0; i < len; i++) {
-			arrayCopy[i] = buf[i]
-		}
-		return arrayCopy.buffer
-	} else {
-		throw new Error('Argument must be a Buffer')
-	}
+	// Otherwise we need to get a proper copy
+	return buf.buffer.slice(buf.byteOffset, buf.byteOffset + buf.byteLength)
 }
